e69fdf8f5a14787239966518326a266d6fc389f3
Colin Bates and Nick Rogers <nick@>
2018-06-05 14:44:39 -0700

    Create fortune site that is 100% accurate and sarcastic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-  
+  $("form#fortune-survey").submit(function(event){
+    event.preventDefault();
+    inputtedName = $("input#enter-name").val();
+
+    var luckyScore = 0;
+    $("input:checkbox[name=lucky]:checked").each(function(){
+      luckyScore += parseInt($(this).val());
+    });
+    $("input:checkbox[name=unlucky]:checked").each(function(){
+      luckyScore -= parseInt($(this).val());
+    });
+    console.log(luckyScore);
+    $(".name").text(inputtedName);
+    if (luckyScore > 20) {
+      console.log("lucky");
+      $("#lucky").show();
+      $("#neutral").hide();
+      $("#unlucky").hide();
+    } else if (luckyScore > -10) {
+      console.log("neutral");
+      $("#lucky").hide();
+      $("#neutral").show();
+      $("#unlucky").hide();
+    } else {
+      console.log("unlucky");
+      $("#lucky").hide();
+      $("#neutral").hide();
+      $("#unlucky").show();
+    }
+    document.getElementById("fortune-survey").reset();
+  });
 });
